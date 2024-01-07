7405609800ea65e5e65e04e7900d99c02e23e40a
Danial Sabry <danielghobrial11@gmail.com>
2024-01-07 22:46:34 +0200

    Added Firebase Database

--- app/src/main/java/com/example/project/Login.java
+++ app/src/main/java/com/example/project/Login.java
@@ -1,24 +1,35 @@
 package com.example.project;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class Login extends AppCompatActivity {
 
-
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        mAuth = FirebaseAuth.getInstance();
         TextView username = findViewById(R.id.email);// Email field
         TextView password = findViewById(R.id.password); // Password field
         Button loginBtn = findViewById(R.id.login_button); // Login Button
@@ -26,23 +37,31 @@ public class Login extends AppCompatActivity {
 
 
         loginBtn.setOnClickListener(view -> {
-            // Username must equal admin
-            // Password must equal 1234
 
-            if(username.getText().toString().trim().equals("albert") && password.getText().toString().trim().equals("1234")){
-                // Correct input, show message then move to home page
+            String user = username.getText().toString().trim();
+            String pass = password.getText().toString().trim();
 
-                Toast.makeText(Login.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
-                new Handler().postDelayed(() -> {
-                    Intent intent = new Intent(Login.this, Home.class);
-                    startActivity(intent);
-                    finish();
-                }, 2000);
-            }
-            else{
-                // Incorrect message, show message and do nothing
-                Toast.makeText(Login.this, "INCORRECT PASSWORD. HINT (albert, 1234)", Toast.LENGTH_SHORT).show();
-            }
+            mAuth.signInWithEmailAndPassword(user, pass)
+                    .addOnCompleteListener(this, task -> {
+                        // Successful login, move user to home page
+                        if(task.isSuccessful()){
+                            Log.d(TAG, "login:success");
+                            FirebaseUser user1 = mAuth.getCurrentUser();
+                            Toast.makeText(Login.this, "Login successful", Toast.LENGTH_SHORT).show();
+                            new Handler().postDelayed(() -> {
+                                Intent intent = new Intent(Login.this, Home.class);
+                                startActivity(intent);
+                                finish();
+                            }, 2000);
+                        }
+                        // Unsuccessful login, display error to user;
+                        else{
+                            Log.w(TAG, "login:failure", task.getException());
+                            Toast.makeText(Login.this, "Login failure", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+
         });
 
         // Switch to register view
--- app/src/main/java/com/example/project/Register.java
+++ app/src/main/java/com/example/project/Register.java
@@ -1,23 +1,36 @@
 package com.example.project;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Register extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
 
         TextView username = findViewById(R.id.email); // Username field
         TextView password = findViewById(R.id.password); // Password field
@@ -29,32 +42,59 @@ public class Register extends AppCompatActivity {
             // Username and Password are extracted and trimmed
             String usernameText = username.getText().toString().trim();
             String passwordText = password.getText().toString().trim();
-            Toast.makeText(Register.this, passwordText, Toast.LENGTH_SHORT).show();
+
+            if(passwordText.length() < 6){
+                Toast.makeText(Register.this, "Password must be 6 characters or more", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            mAuth.createUserWithEmailAndPassword(usernameText, passwordText)
+                    .addOnCompleteListener(this, task -> {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            new Handler().postDelayed(() -> {
+                                Intent intent = new Intent(Register.this, Home.class);
+                                startActivity(intent);
+                                finish();
+                            }, 2000);
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(Register.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
 
             Pattern p1 = Pattern.compile("[a-zA-z]{4,20}"); // Between 4 and 20 alphabetical letters
             Pattern p2 = Pattern.compile("[a-zA-z0-9]{8,}"); // At least 8 alphanumerical characters
 
             // Checking if they match the regex
-            Matcher m1 = p1.matcher(usernameText);
-            Matcher m2 = p2.matcher(passwordText);
+//            Matcher m1 = p1.matcher(usernameText);
+//            Matcher m2 = p2.matcher(passwordText);
 
-            if(m1.matches()){ // Username matches
-                if(m2.matches()){ // Password matches
-                    Toast.makeText(Register.this, "REGISTRATION SUCCESSFUL", Toast.LENGTH_SHORT).show();
-                    // Go to Home Page
-                    new Handler().postDelayed(() -> {
-                        Intent intent = new Intent(Register.this, Home.class);
-                        startActivity(intent);
-                        finish();
-                    }, 2000);
-                }
-                else{
-                    Toast.makeText(Register.this, "PASSWORD MUST BE 8 CHARACTERS OR MORE (alphanumerical characters)", Toast.LENGTH_SHORT).show();
-                }
-            }
-            else{
-                Toast.makeText(Register.this, "USERNAME MUST BE BETWEEN 4 AND 20 CHARACTERS AND CONTAIN ONLY ALPHABETICAL LETTERS", Toast.LENGTH_SHORT).show();
-            }
+//            if(m1.matches()){ // Username matches
+//                if(m2.matches()){ // Password matches
+//                    Toast.makeText(Register.this, "REGISTRATION SUCCESSFUL", Toast.LENGTH_SHORT).show();
+//                    // Go to Home Page
+//                    new Handler().postDelayed(() -> {
+//                        Intent intent = new Intent(Register.this, Home.class);
+//                        startActivity(intent);
+//                        finish();
+//                    }, 2000);
+//                }
+//                else{
+//                    Toast.makeText(Register.this, "PASSWORD MUST BE 8 CHARACTERS OR MORE (alphanumerical characters)", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//            else{
+//                Toast.makeText(Register.this, "USERNAME MUST BE BETWEEN 4 AND 20 CHARACTERS AND CONTAIN ONLY ALPHABETICAL LETTERS", Toast.LENGTH_SHORT).show();
+//            }
+
+
         });
 
         // Switch to login view
